dcd6b1ea465c6db1ade9401564548c2566356127
kc-leung <chiu-leung@Hotmail.com>
2020-05-19 14:30:59 -0400

    project contants keys

--- data/contants.js
+++ data/contants.js
@@ -1,1 +1,8 @@
-const projectArray = [];
+const projectArray = [
+  reactNative - app - PinIT,
+  react - redux - firebase,
+  angular - app - student,
+  angular - github - starring,
+  angular - app - employes,
+  angular - app - comments,
+];
