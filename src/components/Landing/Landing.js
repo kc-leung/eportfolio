a7a571e5ade4b0e631275712112f1a04ce7a4ec1
kc-leung <chiu-leung@Hotmail.com>
2020-05-20 16:01:25 -0400

    landing basic structure and styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,8 +6,18 @@ export default class Landing extends Component {
   render() {
     return (
       <div className="landingContainer">
+        <div className="landingContainer-cover" />
         <Header />
-        <div className="contentContainer">Name</div>
+        <div className="contentContainer">
+          <div className="title">Your Name</div>
+          <div className="separator" />
+          <div className="content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </div>
+        </div>
       </div>
     );
   }
