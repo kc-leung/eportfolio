83045dd6b51f45df3a0e3cb2558210cf431654d6
kc-leung <chiu-leung@Hotmail.com>
2020-05-21 14:56:19 -0400

    color constant and mapping

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import './Projects.scss';
 
+import { colorArray } from '../../data/contants';
+
 export default class Projects extends Component {
   render() {
-    return <div className="projectsContainer">projects content</div>;
+    return (
+      <div className="projectsContainer">
+        <div className="title">P R O J E C T S</div>
+        <div className="projectBanner">
+          {colorArray.map((data, colorIndex) => {
+            return (
+              <div key={colorIndex} style={{ backgroundColor: `${data}` }} />
+            );
+          })}
+        </div>
+      </div>
+    );
   }
 }
--- src/data/contants.js
+++ src/data/contants.js
@@ -1,8 +1,17 @@
-const projectArray = [
-  reactNative - app - PinIT,
-  react - redux - firebase,
-  angular - app - student,
-  angular - github - starring,
-  angular - app - employes,
-  angular - app - comments,
+export const projectArray = [
+  'reactNative - app - PinIT',
+  'react - redux - firebase',
+  'angular - app - student',
+  'angular - github - starring',
+  'angular - app - employes',
+  'angular - app - comments',
+];
+
+export const colorArray = [
+  '#6fc3df',
+  '#8d82c4',
+  '#ec8d81',
+  '#e7b788',
+  '#8ea9e8',
+  '#87c5a4',
 ];
