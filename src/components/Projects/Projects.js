3118df5fd9bf3040bf0e4135a83dea9bffbead11
kc-leung <chiu-leung@Hotmail.com>
2020-05-21 15:10:58 -0400

    project banner style

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -7,11 +7,17 @@ export default class Projects extends Component {
   render() {
     return (
       <div className="projectsContainer">
-        <div className="title">P R O J E C T S</div>
+        <div className="contentContainer">
+          <div className="title">P R O J E C T S</div>
+        </div>
         <div className="projectBanner">
           {colorArray.map((data, colorIndex) => {
             return (
-              <div key={colorIndex} style={{ backgroundColor: `${data}` }} />
+              <div
+                key={colorIndex}
+                className="bannerColor"
+                style={{ backgroundColor: `${data}` }}
+              />
             );
           })}
         </div>
