35c07bdfaf1269ca58e6f7dac923a74023ccfac2
kc-leung <chiu-leung@Hotmail.com>
2020-06-01 15:00:10 -0400

    footer basic structure

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,16 @@ import './Footer.scss';
 
 export default class Footer extends Component {
   render() {
-    return <div className="footerContainer">footer</div>;
+    return (
+      <div className="footerContainer">
+        <div className="contactContainer">
+          <div className="messageContainer">textbox</div>
+          <div className="emailContainer">email</div>
+          <div className="githubContainer">github</div>
+          <div className="phoneContainer">phone</div>
+        </div>
+        <div className="footerContent">rights!</div>
+      </div>
+    );
   }
 }
