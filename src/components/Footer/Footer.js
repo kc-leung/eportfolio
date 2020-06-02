eac5557697bcc77d82fde9399cedaf4ff4264d07
kc-leung <chiu-leung@Hotmail.com>
2020-06-02 15:38:43 -0400

    contact informations

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import './Footer.scss';
 
+import { ReactComponent as EmailIcon } from '../../assets/svg/email.svg';
+import { ReactComponent as GithubIcon } from '../../assets/svg/github.svg';
+import { ReactComponent as PhoneIcon } from '../../assets/svg/phone.svg';
+
 export default class Footer extends Component {
   render() {
     return (
       <div className="footerContainer">
         <div className="contactContainer">
-          <div className="messageContainer">textbox</div>
-          <div className="emailContainer">email</div>
-          <div className="githubContainer">github</div>
-          <div className="phoneContainer">phone</div>
+          <div className="emailContainer">
+            <div className="iconContainer">
+              <EmailIcon className="icon" />
+            </div>
+            <div className="content">
+              <div className="title">Email</div>
+              <div className="info">chiu-leung@Hotmail.com</div>
+            </div>
+          </div>
+          <div className="githubContainer">
+            <div className="iconContainer">
+              <GithubIcon className="icon" />
+            </div>
+            <div className="content">
+              <div className="title">GitHub</div>
+              <div className="info">https://github.com/kc-leung</div>
+            </div>
+          </div>
+          <div className="phoneContainer">
+            <div className="iconContainer">
+              <PhoneIcon className="icon" />
+            </div>
+            <div className="content">
+              <div className="title">Phone</div>
+              <div className="info">(001) 647-390-3654</div>
+            </div>
+          </div>
         </div>
         <div className="footerContent">rights!</div>
       </div>
